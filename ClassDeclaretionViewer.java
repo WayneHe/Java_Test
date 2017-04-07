852c0101bcf13cfe23e3e8490ee58f1fdabb7583
WayneHe <710680178@qq.com>
2017-04-07 21:55:25 +0800

    把各个显示分方法显示-17040701

--- ClassDeclaretionViewer.java
+++ ClassDeclaretionViewer.java
@@ -10,9 +10,19 @@ public class ClassDeclaretionViewer{
         }catch(ClassNotFoundException e){
             System.out.println("未找到该类！");
         }
+        ClassDeclaretionViewer classDeclaretionViewer = new ClassDeclaretionViewer();
+        classDeclaretionViewer.showInit(clazz);
+        classDeclaretionViewer.showTypeVariables(clazz);
+        classDeclaretionViewer.showInterfaces(clazz);
+        classDeclaretionViewer.showSuperClass(clazz);
+        classDeclaretionViewer.showAnnotations(clazz);
+    }
+
+    public void showInit(Class<?> clazz){
         System.out.println("类名为：\n\t" + clazz.getCanonicalName());
         System.out.println("类的修饰符：\n\t" + Modifier.toString(clazz.getModifiers()));
-
+    }
+    public void showTypeVariables(Class<?> clazz){
         TypeVariable<?>[] typeVariables = clazz.getTypeParameters();
         System.out.print("类的泛型参数为：\n");
         if(typeVariables.length != 0){
@@ -22,7 +32,8 @@ public class ClassDeclaretionViewer{
         }else{
             System.out.println("null");
         }
-
+    }
+    public void showInterfaces(Class<?> clazz){
         Type[] interfaces = clazz.getGenericInterfaces();
         System.out.print("类所实现的接口为：\n");
         if(interfaces.length != 0){
@@ -32,7 +43,9 @@ public class ClassDeclaretionViewer{
         }else{
             System.out.println("null");
         }
+    }
 
+    public void showSuperClass(Class<?> clazz){
         Type superClass = clazz.getGenericSuperclass();
         System.out.print("类的直接继承类为：\n\t");
         if(superClass != null){
@@ -40,7 +53,9 @@ public class ClassDeclaretionViewer{
         }else{
             System.out.println("null");
         }
+    }
 
+    public void showAnnotations(Class<?> clazz){
         Annotation[] annotations = clazz.getAnnotations();
         System.out.print("类的注解为：\n\t");
         if(annotations.length != 0){
